5a1a4a2711a706ce032814611b251480f336aab8
Cory Ribson & andrew david burt <student@epicodus.com>
2015-04-27 14:23:20 -0700

    validate type of triangle.

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -1,1 +1,33 @@
+class Triangles
+  define_method(:initialize) do |side1, side2, side3|
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
 
+  # define_method(:is_triangle?) do
+  #   if @side1.+(@side2) >= @side3 && @side1.+(@side3) >= @side2 && @side2.+(@side3) >= @side1
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
+
+  define_method(:sides) do
+    return @side1, @side2, @side3
+  end
+
+  define_method(:typetriangle) do
+    if @side1.+(@side2) >= @side3 && @side1.+(@side3) >= @side2 && @side2.+(@side3) >= @side1
+      if @side1 == @side2 && @side1 == @side3
+        "equilateral"
+      elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+        "isosceles"
+      elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+        "scalene"
+      end
+    else
+       "Is not a triangle"
+    end
+  end
+end
--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -5,16 +5,44 @@ require('triangles')
 describe (Triangles) do
   describe ("#initialize")  do
     it("initializes triangle") do
-      expect(Triangles.new(3, 3, 3)).to(eq([3, 3, 3]))
-    end
-    it("checks that it's a valid triangle") do
-      expect(Triangles.new(2, 2, 8)).to(eq(false))
+      my_triangle = Triangles.new(3, 3, 3)
+      expect(my_triangle.sides()).to(eq([3, 3, 3]))
     end
   end
-  describe ("#type") do
+
+  # describe("#is_triangle?") do
+  #   it("checks that it's a valid triangle") do
+  #     my_triangle = Triangles.new(2, 2, 8)
+  #     expect(my_triangle.is_triangle?()).to(eq(false))
+  #   end
+  # end
+
+  describe ("#typetriangle") do
     it("checks type of triangle for equilaterals") do
       my_equilateral = Triangles.new(3, 3, 3)
-      expect(Triangles.type()).to(eq("equilateral"))
+      expect(my_equilateral.typetriangle()).to(eq("equilateral"))
     end
   end
+
+  describe ("#typetriangle") do
+    it("checks type of triangle for isosceles") do
+      my_isosceles = Triangles.new(3, 2, 2)
+      expect(my_isosceles.typetriangle()).to(eq("isosceles"))
+    end
+  end
+
+  describe ("#typetriangle") do
+    it("checks type of triangle for scalene") do
+      my_scalene = Triangles.new(1, 2, 3)
+      expect(my_scalene.typetriangle()).to(eq("scalene"))
+    end
+  end
+
+  describe ("#typetriangle") do
+    it("checks type of triangle for invalid shape") do
+      my_weirdshape = Triangles.new(2, 2, 8)
+      expect(my_weirdshape.typetriangle()).to(eq("Is not a triangle"))
+    end
+  end
+
 end
